b39e72a9c407e4abbfb26d60e4706fe425cf500c
Mohit Ingale <mohitingale5@gmail.com>
2021-04-14 17:02:06 -0700

    Priviledge speration for socket server code

---
+++ Assignment_1/server.c
@@ -0,0 +1,79 @@
+// Server side C/C++ program to demonstrate Socket programming
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define PORT 8080
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[102] = {0};
+    char *hello = "Hello from server";
+
+    printf("execve=0x%p\n", execve);
+    printf("Parent UID=%d\n", getuid());
+
+    // Creating socket file descriptor
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Attaching socket to port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                    (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    pid_t id = fork();
+    if(id ==0){
+        if(setuid(65534)==-1){
+            perror("Unable to set user for child process");
+            exit(EXIT_FAILURE);
+        }
+        printf("Child UID=%d\n", getuid());
+        valread = read( new_socket , buffer, 1024);
+        printf("%s\n",buffer );
+        send(new_socket , hello , strlen(hello) , 0 );
+        printf("Hello message sent\n");
+    }
+    else{
+        int status = 0;
+        while ((wait(&status) > 0));
+    }
+    return 0;
+}
